7cec14a9efb73eb671655f4b356aeb6656855c32
gtovar1985 <gtovar@incaztech.com>
2013-11-29 11:41:35 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storeincaztech::Application.config.secret_key_base = '2ad6a550eea9593bc64f2aa8a69be2a28e7cc80470b869f696abbbe8902a738b0efdb3984dd053a43f7248aad81999608570ef52ffe476f382e54c978f4a65a2'
